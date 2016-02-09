bc5edca8f37e345008f8d947b4a9a0727efcbbe0
Jerome Gero <JeromeRGero@gmail.com>
2016-02-09 09:08:44 -0500

    Homework
    I think it's ... good?

--- Book.java
+++ Book.java
@@ -11,6 +11,9 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int barrowed;
 
     /**
      * Set the author and title fields when this object
@@ -20,7 +23,73 @@ class Book
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = 0;
+        refNumber = "";
+        barrowed = 0;
     }
 
     // Add the methods here ...
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+    public void getBarrowed()
+    {
+        System.out.println(barrowed);
+    }
+    
+    public void printAuthor()
+    {
+        System.out.println("Author: " + author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println("Title: " + title);
+    }
+    
+    public void barrowed()
+    {
+        barrowed += 1;
+    }
+    
+    public void getTheDets()
+    {
+        System.out.println("Title: " + title + 
+        " Author: " + author + " Pages: " + pages);
+        System.out.println("This book has been barrowed" + barrowed + "times!");
+        if (refNumber.length() != 0)
+        {
+            System.out.println(" Reference number: " + refNumber);
+        }
+        else
+        {
+            System.out.println("\nZzz...");
+        }
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        if(ref.length() > 3)
+        {
+            System.out.print("ERROR: Input must be 3 Chara or more.");
+        }
+        else
+        {
+            refNumber = ref;
+        }
+    }
+    
+    
 }
